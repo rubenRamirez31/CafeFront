2aecaaba92b3380d1fb1c6f74224da138b6f208b
rubenRamirez31 <l20te0221@teziutlan.tecnm.mx>
2023-12-01 14:51:46 -0600

    pagina de registrar terminada

---
+++ .next/static/webpack/app/Registrar/page.35780d8a7e5ac0f2.hot-update.js
@@ -0,0 +1,21 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/Registrar/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cruben%5CDownloads%5CCafeFront%5Capp%5Cstyles.module.css&server=false!":
+/*!**************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cruben%5CDownloads%5CCafeFront%5Capp%5Cstyles.module.css&server=false! ***!
+  \**************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.t.bind(__webpack_require__, /*! ./app/styles.module.css */ \"(app-pages-browser)/./app/styles.module.css\", 23))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz1DJTNBJTVDVXNlcnMlNUNydWJlbiU1Q0Rvd25sb2FkcyU1Q0NhZmVGcm9udCU1Q2FwcCU1Q3N0eWxlcy5tb2R1bGUuY3NzJnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz83YWNiIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiQzpcXFxcVXNlcnNcXFxccnViZW5cXFxcRG93bmxvYWRzXFxcXENhZmVGcm9udFxcXFxhcHBcXFxcc3R5bGVzLm1vZHVsZS5jc3NcIikiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=C%3A%5CUsers%5Cruben%5CDownloads%5CCafeFront%5Capp%5Cstyles.module.css&server=false!\n"));
+
+/***/ })
+
+});
--- app/Registrar/page.tsx
+++ app/Registrar/page.tsx
@@ -1,8 +1,8 @@
-'use client'
-import { signIn } from 'next-auth/react';
-import Link from 'next/link';
+'use client';
 import { useRouter } from 'next/navigation';
-import React, { useState } from 'react'
+import { signIn, useSession } from 'next-auth/react';
+import Link from 'next/link';
+import React, { FormEvent, useState } from 'react'
 import { useForm } from 'react-hook-form';
 import { ILogin } from '../models/ILogin';
 import Swal from 'sweetalert2';
@@ -21,41 +21,62 @@ import {
     from 'mdb-react-ui-kit';
 
 const RegistrarPage = () => {
-    const [errors, SetError] = useState<string>("");
-    const { handleSubmit, register } = useForm<ILogin>();
+    const { data: session, status } = useSession();
     const router = useRouter();
 
-    const onSubmit = handleSubmit(async (formData, event) => {
-        event?.preventDefault();
+    async function onSubmit(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault()
 
+        const formData = new FormData(event.currentTarget)
 
-        const responseLogin = await signIn("credentials", {
-            username: formData.username,
-            password: formData.password,
-            redirect: false,
+        const response = await fetch('http://localhost:8080/Usuarios/Registrar', {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                nombre: formData.get("nombre"),
+                apePat: formData.get("apePat"),
+                apeMat: formData.get("apeMat"),
+                correo: formData.get("correo"),
+                pwd: formData.get("pwd"),
+                rol: {
+                    idRol: 3,
+                    nombre: "Cliente"
+                },
+                numTelefono: formData.get("numTelefono"),
+            })
         });
 
-        if (responseLogin?.error) {
+        const data = await response;
+
+        if (data.status == 200) {
+
             Swal.fire({
-                title: "¡Ups!",
-                text: "Parece que tu correo o contraseña son incorrectos",
-                icon: "error",
+                title: "Excelente",
+                text: "Te registaste correctamente",
+                icon: "success"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    router.push('/Login');
+                }
+            })
+
+        } else {
+            Swal.fire({
+                title: "Atención",
+                text: "Este correo ya esta en uso",
+                icon: "warning"
             });
-            return;
+
+            console.log(response)
         }
-
-        router.push("..");
-    });
-
+    }
     return (
         <>
-
-            <MDBContainer className="my-4">
-
+            <MDBContainer className="my-4 container-fluid">
                 <MDBCard>
                     <MDBRow className='g-0'>
-
-
                         <MDBCol md='6'>
                             <MDBCardBody className='d-flex flex-column'>
 
@@ -65,7 +86,7 @@ const RegistrarPage = () => {
                                 <div className='d-flex justify-content-center'>
                                     <h5 className="fw-normal my-1 pb-3" style={{ letterSpacing: '1px' }}>Crea una cuenta para ingresar</h5>
                                 </div>
-                                <form onSubmit={onSubmit}>
+                                <form onSubmit={onSubmit} >
 
                                     <div className='d-lg-flex justify-content-lg-between'>
                                         <div className="col-sm-12 col-lg-3 my-3 ">
@@ -98,29 +119,24 @@ const RegistrarPage = () => {
                                     <div className='d-flex justify-content-between'>
                                         <div className="col-5 my-3 ">
                                             <label htmlFor="password">Contraseña</label>
-                                            <input type="password" className="form-control form-control-sm" />
+                                            <input type="password" name='pwd' className="form-control form-control-sm" required />
                                         </div>
                                         <div className="col-5 my-3 ">
                                             <label htmlFor="password2">Confirma tu contraseña</label>
-                                            <input type="password" className="form-control form-control-sm" />
+                                            <input type="password" className="form-control form-control-sm" required />
                                         </div>
                                     </div>
-
-
                                     <div >
                                         <button type="submit" style={{ width: '100%' }} className={styles.btnprincipal}>Registrarme</button>
                                     </div>
                                 </form>
                             </MDBCardBody>
                         </MDBCol>
-
                         <MDBCol md='6'>
                             <MDBCardImage src='/CafeRegistro.jpg' alt="login form" className='rounded-start w-100' />
                         </MDBCol>
-
                     </MDBRow>
                 </MDBCard>
-
             </MDBContainer>
 
         </>
